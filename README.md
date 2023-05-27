4f2330da63b5f419035196e16f791ed387471b88
brijesh987 <brijeshghonia987@gmail.com>
2023-05-27 12:23:35 +0530

    Added some routes

--- README.md
+++ README.md
@@ -2,15 +2,19 @@
 
 This is a book record management API Backend for the management of book records.
 
+# API Documentation Link
+
+https://documenter.getpostman.com/view/27656442/2s93m7WMGS
+
 # Routes and Endpoints
 
 ## /users
-POST: Create a new user
-GET: Get all list of users
+POST: Create a new user ✅
+GET: Get all list of users ✅
 
 ## /users/{id}
-GET: Get a user by ID
-PUT: Update a user by ID
+GET: Get a user by ID ✅
+PUT: Update a user by ID ✅
 DELETE: Delete a user by ID (check if he/she still has an issued book or is there any fine to be paid)
 
 ## /users/subscription-details/{id}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express');
+const express = require("express");
+const { users } = require("./data/users.json");
 
 const app = express();
 
@@ -9,6 +10,87 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.status(200).json({
         message: "Server is running"
+    });
+});
+
+//Get all list of users
+app.get('/users', (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: users
+    })
+});
+
+//Get a user by ID
+app.get('/users/:id', (req, res) => {
+    const { id } = req.params
+    const user = users.find((each) => each.id === id);
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found"
+        });
+    }
+    res.status(200).json({
+        success: true,
+        data: users
+    })
+});
+
+//Create a new user
+app.post('/users', (req, res) => {
+    const { id, name, surname, email, subscriptionType, subscriptionDate } = req.body;
+
+    const user = users.find((each) => each.id === id);
+
+    if (user) {
+        return res.status(404).json({
+            success: false,
+            message: "User exists with this ID"
+        });
+    }
+
+    users.push({
+        id,
+        name,
+        surname,
+        email,
+        subscriptionType,
+        subscriptionDate
+    });
+
+    return res.status(201).json({
+        success: true,
+        data: users
+    });
+});
+
+//Update a user details by ID
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { data } = req.body;
+    const user = users.find((each) => each.id === id);
+
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User Not Found"
+        });
+    }
+
+    const updatedUser = users.map((each) => {
+        if (each.id === id) {
+            return {
+                ...each,
+                ...data
+            };
+        }
+        return each;
+    });
+
+    return res.status(200).json({
+        success: true,
+        data: updatedUser
     });
 });
 
